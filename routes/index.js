58020bb002911b9e945531717de87eed2dbc3074
5addam <saddam.mughal@protonmail.com>
2021-07-30 16:57:25 +0500

    navbar added to parent layout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+// require controller modules
+const main_controller = require('../controllers/mainController');
+
+/**
+ * ----------------------------GET REQUESTS-----------------------------------------
+ */
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', main_controller.index);
+
+/* GET Sign up page */
+router.get('/sign-up', main_controller.sign_up_get);
+
+/* GET Login page */
+router.get('/login', main_controller.login_get);
+
 
 module.exports = router;
